ce44705281eded31498136fd826f2c94045a189e
Jonathan Cheng <jcheng461@gmail.com>
2022-05-07 23:42:14 +0900

    add method to return num of words in entry. Add methods & functions to return num of vowels & consonants.

--- src/entries.js
+++ src/entries.js
@@ -1,8 +1,97 @@
+// Business Logic
 export default function Entry(journalTitle, journalEntry) {
   this.journalTitle = journalTitle;
   this.journalEntry = journalEntry;
 }
 
+// Utility Logic
+// Remove all non-alphanumeric characters from string
+function removePunctuation(string) {
+  const stripped = string.replace(/[^a-z0-9 ]/gi, "");
+  console.log("Punctuation removed: " + stripped);
+  return stripped;
+}
+
+function isVowel(character) {
+  if (
+    character === "a" ||
+    character === "e" ||
+    character === "i" ||
+    character === "o" ||
+    character === "u"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isConsonant(character) {
+  if (
+    character !== "a" &&
+    character !== "e" &&
+    character !== "i" &&
+    character !== "o" &&
+    character !== "u" &&
+    character.match(/[a-z]/)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Counts number of vowels.
+function countVowels(string) {
+  // Removes punctuation
+  const entryNoPunctuation = removePunctuation(string);
+  // Lowercases all remaining characters
+  const lowercaseEntry = entryNoPunctuation.toLowerCase();
+  // Loops through modified string and tallies up vowels.
+  let vowels = 0;
+  for (let i = 0; i < lowercaseEntry.length; i++) {
+    if (isVowel(lowercaseEntry[i]) === true) {
+      vowels++;
+    }
+  }
+  console.log("There are " + vowels + " vowels in the string.");
+  return vowels;
+}
+
+// Count consonants.
+function countConsonants(string) {
+  // Removes punctuation
+  const entryNoPunctuation = removePunctuation(string);
+  // Lowercases all remaining characters
+  const lowercaseEntry = entryNoPunctuation.toLowerCase();
+  // Loops through modified string and tallies up consonants.
+  let consonants = 0;
+  for (let i = 0; i < lowercaseEntry.length; i++) {
+    if (isConsonant(lowercaseEntry[i]) === true) {
+      consonants++;
+    }
+  }
+  console.log("There are " + consonants + " vowels in the string.");
+  return consonants;
+}
+
 Entry.prototype.showEntry = function () {
   return "Show journal entry here!";
+};
+
+Entry.prototype.numberWords = function () {
+  const separateWords = this.journalEntry.split(" ");
+  return separateWords.length;
+};
+
+Entry.prototype.numberVowels = function () {
+  return "Number of vowels in entry: ";
+};
+
+Entry.prototype.numberConsonants = function () {
+  return "Number of consonants in entry: ";
+};
+
+Entry.prototype.getTeaser = function () {
+  return "First sentence, or first 8 words of entry: ";
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+// User interface logic
+
 import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import Entry from "./entries.js";
+
+// Utility Logic
+// Remove all non-alphanumeric characters from string
+function removePunctuation(string) {
+  const stripped = string.replace(/[^a-z0-9 ]/gi, "");
+  console.log("Punctuation removed: " + stripped);
+  return stripped;
+}
 
 $(document).ready(function () {
   $("#journal-form").submit(function (event) {
@@ -11,6 +21,14 @@ $(document).ready(function () {
     const journalEntry = $("#entry").val();
     const journal = new Entry(journalTitle, journalEntry);
     const entry = journal.showEntry();
+    const wordsOnly = journal.numberWords();
+    const entryNoPunctuation = removePunctuation(journalEntry);
+    console.log("Entry with no punctuation: " + entryNoPunctuation);
+    const lowercaseEntry = entryNoPunctuation.toLowerCase();
+    console.log("Entry with characters lowercased: " + lowercaseEntry);
+
+    // Display to html
     $("#showEntry").append("<p>" + entry + "</p>");
+    $("#showEntry").append("<p> Entry has: " + wordsOnly + " words.</p>");
   });
 });
